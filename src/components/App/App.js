d1c98edbf7b180b1357f3683219a9fc5e43e63b8
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-08-31 20:53:50 -0400

    router (ex5)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
+import ArtistRoute from "./ArtistRoute";
 import GlobalStyles from "./GlobalStyles";
+
+const DEFAULT_ARTIST_ID = "0U04xAocTetRnA7v3TYQ2i";
 
 const App = () => {
   return (
     <Router>
       <GlobalStyles />
-      TODO
+      <Switch>
+        <Route path={"/artists/:id"}>
+          <ArtistRoute />
+        </Route>
+        <Route exact={true} path={"/"}>
+          <Redirect to={{ pathname: `/artists/${DEFAULT_ARTIST_ID}` }} />
+        </Route>
+      </Switch>
     </Router>
   );
 };
