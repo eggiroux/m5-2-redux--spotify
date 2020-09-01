903cb7414322d944830c91802dc5098022ae7070
Etienne Giroux <etienneg@Etiennes-MacBook-Pro.local>
2020-08-31 20:39:35 -0400

    Setup redux store (ex4)

---
+++ src/reducers/artists-reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  currentArtist: null,
+  status: "idle",
+};
+
+export default function artistsReducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
